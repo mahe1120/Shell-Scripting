a2e46a55b146b8fe5df6e4b8d541ae043b8b1479
mahender <gmahe139@gmail.com>
2026-02-20 17:47:59 +0530

    script added

--- 07-Installscript.sh
+++ 07-Installscript.sh
@@ -33,11 +33,11 @@ CHECK_ROOT
 
 echo "script started executing at :: $TIMESTAMP"
 
-dnf list installed mysql # To Check if mysql is installed or not
+dnf list installed mysql &>>$LOG_FILE_NAME   # To Check if mysql is installed or not
 if [ $? -ne 0 ]
 then
     echo "MySQL is not installed."
-    dnf install mysql
+    dnf install mysql &>>$LOG_FILE_NAME
     VALIDATE $? "Installing MYSQL"
 fi
 
