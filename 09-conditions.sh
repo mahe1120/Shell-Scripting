0c010c8ca696b44304d0806983bbd5238ccf8d51
mahender <gmahe139@gmail.com>
2026-02-20 18:17:14 +0530

    script added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
 then
-    echo "Given number is greater than $NUMBER"
+    echo "Given number is greater than 10"
 else
-    echo "Given number is less than $NUMBER"
+    echo "Given number is less than 10"
 fi
