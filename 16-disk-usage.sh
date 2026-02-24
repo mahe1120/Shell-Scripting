ded89495bfaba0c5335f44d5e793430d304ccf50
mahender <gmahe139@gmail.com>
2026-02-24 19:08:24 +0530

    Disk usage script added

---
+++ 16-disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
+
+while read -r line
+do 
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F " " '{print NF}')
+    echo "Partition:$PARTITION , Usage:$USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "High Disk Usage on partition:$PARTITION, Usage:$USAGE"
+    fi
+done <<< $DISK_USAGE
+
+
