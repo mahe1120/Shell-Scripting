c3f91fa58400df43f9e5e29151af0e0ff09b01c6
mahender <gmahe139@gmail.com>
2026-02-24 19:13:52 +0530

    Disk usage script added

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -10,7 +10,7 @@ do
     echo "Partition:$PARTITION , Usage:$USAGE"
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "High Disk Usage on partition:$PARTITION, Usage:$USAGE"
+        echo "High Disk Usage on partition:$PARTITION Usage is:$USAGE \n"
     fi
 done <<< $DISK_USAGE
 
